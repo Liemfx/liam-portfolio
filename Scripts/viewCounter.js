858a781bae9a6f2eccc19535dcaa1396193367eb
Liemfx <liam.mardnas07@gmail.com>
2025-03-08 14:44:43 +0100

    update

--- Scripts/viewCounter.js
+++ Scripts/viewCounter.js
@@ -3,6 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebas
 import {
   getDatabase,
   ref,
+  set,
   onValue,
 } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
 
@@ -21,6 +22,27 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
+function get_viewers_ip(json) {
+  const viewers_ip = json.ip;
+  // count view with ip
+  count_view(viewers_ip);
+}
+
+function count_view(viewers_ip) {
+  const ip_to_string = viewers_ip.replace(/\./g, "-");
+
+  set(ref(database, "page_views/" + ip_to_string), {
+    viewers_ip: viewers_ip,
+  });
+}
+
+// Fetch the IP address using the ipify API
+fetch("https://api.ipify.org?format=json")
+  .then((response) => response.json())
+  .then((data) => get_viewers_ip(data))
+  .catch((error) => console.error("Error fetching IP address:", error));
+
+// Update the view count in real-time
 onValue(ref(database, "page_views"), (snapshot) => {
   let views = 0;
   snapshot.forEach(() => {
